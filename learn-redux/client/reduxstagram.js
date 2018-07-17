bb6803d113cbd3028df21d5241eb9e551c601526
Michał Nędzka <michael.nedzka@gmail.com>
2018-07-17 21:36:30 +0200

    basic setup and fire off project in browser

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from "react";
+
+import { render } from "react-dom";
+
+import Main from "./data/components/Main";
+
+//Import css
+import css from "./styles/style.styl";
+
+render(<Main />, document.getElementById("root"));
