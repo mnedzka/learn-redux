7b0b6c72368b13cea157d6b1469be329f172679c
Michał Nędzka <michael.nedzka@gmail.com>
2018-07-17 23:06:20 +0200

    Add reactRouter + singleand Photogrid components

---
+++ learn-redux/client/data/components/Single.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Single extends Component {
+  render() {
+    return <div className="single-photo">i'm single</div>;
+  }
+}
+
+export default Single;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,8 +3,24 @@ import React from "react";
 import { render } from "react-dom";
 
 import Main from "./data/components/Main";
+import Single from "./data/components/Single";
+import PhotoGrid from "./data/components/PhotoGrid";
 
 //Import css
 import css from "./styles/style.styl";
 
-render(<Main />, document.getElementById("root"));
+//Import React Router
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}>.</IndexRoute>
+      <Route path="/view/:postID" component={Single}>
+        .
+      </Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
