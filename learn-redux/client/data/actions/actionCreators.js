ef2e9ed92712e27d6bedf262501b205a11e3ebfd
Michal Nedzka <michael.nedzka@gmail.com>
2018-07-19 21:12:24 +0200

    Add actions creators

---
+++ learn-redux/client/data/actions/actionCreators.js
@@ -0,0 +1,19 @@
+//increment
+function increment(index) {
+    return {
+        type: `INCREMENT_LIKES`,
+        index,
+    }
+}
+
+//add comment
+function addComment(postId, author, comment) {
+    return {
+        type: `ADD_COMMENT`,
+        postId, 
+        author, 
+        comment,
+    }
+}
+
+//remove comment
