391ac01b55cc260b1cd3aaf86c6e40a41e95955c
Michal Nedzka <michael.nedzka@gmail.com>
2018-07-20 23:59:22 +0200

    Displaying and adding comments

--- learn-redux/client/data/components/Photo.js
+++ learn-redux/client/data/components/Photo.js
@@ -4,11 +4,11 @@ import CSSTransitionGroup from "react-addons-css-transition-group";
 
 export default class Photo extends Component {
   render() {
-    // const {post, i, comments} = this.props;
+    const {post, i, comments} = this.props;
     return (
       <figure className="grid-figure">
           <div className="grid-photo-wrap">
-            <Link to={`/view/${this.props.post.code}`}>
+            <Link to={`/view/${post.code}`}>
               <img src={post.display_src} alt={post.caption} className="grid-photo" />
             </Link>
 
--- learn-redux/client/data/components/comments.js
+++ learn-redux/client/data/components/comments.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Comments extends Component {
+  renderComment(comment, i) {
+    <div className="comment" key={i}>
+      <p>
+        <strong>{comment.user}</strong>
+        {comment.text}
+        <button className="remove-comment">&times;</button>
+      </p>
+    </div>
+  }
   render() {
     return (
-      <div className="comment">i'm a comment</div>
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden />
+        </form>
+      </div>
     )
   }
 }
