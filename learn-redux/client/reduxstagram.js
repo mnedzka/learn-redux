8033e3a4abcb2ed6b7976117ae6f6429a7bf1c5d
Michal Nedzka <michael.nedzka@gmail.com>
2018-07-21 10:24:41 +0200

    Add sentry io error handling

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "react-dom";
+import { render } from 'react-dom';
 
-// import Components
-import App from "./data/components/App";
-import Single from "./data/components/Single";
-import PhotoGrid from "./data/components/PhotoGrid";
+// Import css
+import css from './styles/style.styl';
 
-//Import css
-import css from "./styles/style.styl";
+// Import Components
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-//Import React Router
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
-import {Provider} from 'react-redux';
-import store, {history} from './store';
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'as09d8f09'
+  }
+}).install();
+
+logException(new Error('Incomplete Data!'), {
+  email: 'michael.nedzka@gmail.com'
+});
+
+Raven.showReportDialog();
 
 const router = (
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
-        <Route path="/view/:postID" component={Single}>
-          .
-        </Route>
-      </Route> 
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
     </Router>
   </Provider>
-);
+)
 
-render(router, document.getElementById("root"));
-
+render(router, document.getElementById('root'));
